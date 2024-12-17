59a3eeaba980b685a7b6735ac138d99ddf0386e6
AgostinoCaruso <agostinocaruso321@gmail.com>
2024-12-17 16:01:30 +0100

    set up the basic form layout

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,25 @@
 import articleDB from "../articleDB";
 
-function Main(){
-    return(
-        <main className=" container d-flex justify-content-center">
-            <h2>Lista forms</h2>
+function Main() {
+    return (
+        <main className=" container my-5">
+            <div className="d-flex justify-content-center row">
+                {
+                    articleDB.map((ele) => {
+                        return (
+                            <div className="card myCard col-3 m-3" key={ele.id} >
+                                <img className="card-img-top" src="https://picsum.photos/200/200" alt="Card image cap" />
+                                <div className="card-body">
+                                    <h5 className="card-title">{ele.title}</h5>
+                                    <p className="card-text">{ele.author}</p>
+                                    <button className="btn btn-primary mx-3">Go somewhere</button>
+                                    <button className="btn btn-danger">Delete</button>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </main>
     );
 }
